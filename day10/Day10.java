73c5f7b5d2e110403a63234cfc200e5cffbfec33
vburalla <100727742+vburalla@users.noreply.github.com>
2023-10-05 16:56:51 +0000

    day10_

--- day10/Day10.java
+++ day10/Day10.java
@@ -17,15 +17,23 @@ public class Day10 {
     private static void executeInstructions(List<String> instructions) {
 
         Integer value = 1;
+        int step = 1;
         String[] instructionParts;
+        System.out.println("X begins with value: " + value);
         for(String instruction : instructions) {
+           
             instructionParts = instruction.split(" ");
-            if(!instructionParts[0].equals("noop")){
-
+            if(!instructionParts[0].equals("noop")) {
+                for(int i=0;i<2;i++){
+                    values.add(value);
+                }
+                value+= Integer.valueOf(instructionParts[1]);
             }
-            values.add(value);
-        }
+            System.out.println("At end of step " + step + ", X value is: " + value);
+             values.add(value);
+             step++;
 
+        }
     }
     
 }
