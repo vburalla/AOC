50bbf37761826cf4448099d3bdd95bf930b5b7c6
vburalla <100727742+vburalla@users.noreply.github.com>
2023-10-05 14:45:56 +0000

    finish part1 and part2

--- day9/Day9.java
+++ day9/Day9.java
@@ -6,43 +6,62 @@ import java.util.HashSet;
 class Day9 {
 
     private static Set<String> visitedPositions = new HashSet<>();
-    private static Position headPosition;
-    private static Position tailPosition;
+    private static Position[] knotPositions;
+
+    private static Position[] createFoldedRope(int knots) {
+
+        Position[] rope = new Position[knots + 1];
+        for(int i = 0; i < knots + 1; i++) {
+            rope[i] = new Position(0, 0);
+        }
+        return rope;
+    }
 
     public static void main(String[] args) {
 
-        List<String> instructions = ReadFiles.getInputData("day9/test.txt");
-        headPosition = new Position(0, 0);
-        tailPosition = new Position(0, 0);
-        move(instructions);
+        List<String> instructions = ReadFiles.getInputData("day9/input1.txt");
+        knotPositions = createFoldedRope(1);
+        move(instructions, 1);
+        System.out.println("Part 1: " + visitedPositions.size());
+        knotPositions = createFoldedRope(9);
+        move(instructions, 9);
+        System.out.println("Part 2: " + visitedPositions.size());
     }
 
-    private static void move(List<String> movements) {
-        
-        visitedPositions.add(tailPosition.toString());
+    private static void move(List<String> movements, int monitorizedKnot) {
+        visitedPositions.clear();
+        visitedPositions.add(knotPositions[monitorizedKnot].toString());
         for(String movement : movements) {
-            moveRope(movement);
+            moveRope(movement, monitorizedKnot);
         }
     }
 
-    private static void moveRope(String movement) {
+    private static void moveRope(String movement, int monKnot) {
 
         String[] movementParts = movement.split(" ");
         int steps = Integer.parseInt(movementParts[1]);
-        for(int i=0; i<steps;i++){
+        for(int i=0; i < steps; i++) {
             Position stepPos = getDisplacement(movementParts[0]);
-            headPosition.changePosition(stepPos);
-            moveTail();
+            knotPositions[0].changePosition(stepPos);
+            moveTailKnots();
+            visitedPositions.add(knotPositions[monKnot].toString());
         }
     }
 
-    private static void moveTail() {
+    private static void moveTailKnots() {
 
-        var distance = tailPosition.getDistanceTo(headPosition);
-        if( Math.abs(Math.max(distance.getX(), distance.getY())) > 1) {
-            
+        for (int i = 1; i < knotPositions.length; i++) {
+            var distance = knotPositions[i].getDistanceTo(knotPositions[i-1]);
+            if( Math.max(Math.abs(distance.getX()), Math.abs(distance.getY())) > 1) {
+                Position tailDiplacement = new Position(getNewCoordinate(distance.getX()),getNewCoordinate(distance.getY()));
+                knotPositions[i].changePosition(tailDiplacement);
+            }        
         }
+    }
 
+    private static int getNewCoordinate(int coordinateDifference) {
+
+        return coordinateDifference == 0? 0 : -1 * coordinateDifference + (coordinateDifference / 2);
     }
 
     private static Position getDisplacement(String direction) {
