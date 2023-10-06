5dd171f33e32fb225fd9c677cfccbf1a61658630
vburalla <100727742+vburalla@users.noreply.github.com>
2023-10-06 13:29:15 +0000

    finish day11

--- day11/Day11.java
+++ day11/Day11.java
@@ -3,47 +3,120 @@ package day11;
 import utils.ReadFiles;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day11 {
 
     public static void main(String[] args) {
 
-        List<String> information = ReadFiles.getInputData("day11/test.txt");
+        Monkey[] monkeysList = getMonkeys(ReadFiles.getInputData("day11/input1.txt"));
+        playWithMonkeys(monkeysList.clone(), 20, true, 1);
+        playWithMonkeys(monkeysList.clone(), 10000, false, 2);
     }
 
-    public List<Monkey> getMonkeys(List<String> monkeyInfo) {
+    public static Monkey[] getMonkeys(List<String> monkeyInfo) {
 
         List<Monkey> monkeys = new ArrayList<>();
-        int line = 0;
         String lineText = "";
+        int id = 0;
         do {
-            Monkey monkey = new Monkey();
-            while(line < monkeyInfo.size() && !(lineText = monkeyInfo.get(line)).equals("")) {
+            Monkey monkey = new Monkey(id);
+            while(monkeyInfo.size() > 0 && !(lineText = monkeyInfo.remove(0)).equals("")) {
                 if(lineText.contains("Starting items:")) {
-
+                    setMonkeyItems(lineText, monkey);
                 } else if(lineText.contains("Operation:")){
-
+                    setMonkeyOpeartion(lineText, monkey);
                 } else if(lineText.contains("Test")){
-
+                    setMonkeyTestValue(lineText, monkey);
                 } else if(lineText.contains("If true")) {
-
+                    setMonkeyIfTestTrue(lineText, monkey);
                 } else if(lineText.contains("If false")) {
-
+                    setMonkeyIfTestFalse(lineText, monkey);
                 }
-                line++;
             }
             monkeys.add(monkey);
+            id++;
         } while (monkeyInfo.size()>0);
+        return monkeys.toArray(new Monkey[monkeys.size()]);
     }
 
-    private void createMonkeyWithGivenInfo(List<String> info) {
-        Monkey monkey = new Monkey();
+    private static void setMonkeyItems(String text, Monkey monk) {
+
+        var a = Arrays.asList(text.replace("Starting items ", "").split(": ")[1].split(", "))
+        .stream().map(Long::valueOf).collect(Collectors.toList());
+        monk.setItems(a);
+    }
+
+    private static void setMonkeyOpeartion(String text, Monkey monk) {
+
+        var splittedInfo = text.split(" ");
         
+        String op = splittedInfo[splittedInfo.length-1];
+        if(op.equals("old")){
+            monk.setOperator("^2");
+        } else {
+            monk.setOperator(splittedInfo[splittedInfo.length-2]);
+            monk.setOperationAmount(Integer.parseInt(op));
+        }
     }
 
+    private static void setMonkeyTestValue(String text, Monkey monk) {
 
-        return monkeys;
+        var splittedInfo = text.split(" ");
+        monk.setTest(Integer.parseInt(splittedInfo[splittedInfo.length-1]));
     }
 
+    private static void setMonkeyIfTestTrue(String text, Monkey monk) {
+
+        var splittedInfo = text.split(" ");
+        monk.setMonkeyIfDivisible(Integer.parseInt(splittedInfo[splittedInfo.length-1]));
+    }
+
+    private static void setMonkeyIfTestFalse(String text, Monkey monk) {
+
+        var splittedInfo = text.split(" ");
+        monk.setMonkeyIfNotDivisible(Integer.parseInt(splittedInfo[splittedInfo.length-1]));
+    }
+
+    private static void playWithMonkeys(Monkey[] monkeyPlayers, int rounds, boolean divideWorryLevel, int part) {
+
+        long mcm = calculateLCM(monkeyPlayers);
+
+        for(int i=0; i<rounds; i++) {
+            for(int j=0; j<monkeyPlayers.length; j++) {
+                while(monkeyPlayers[j].getItemsAmount() > 0) {
+                    Item item = monkeyPlayers[j].inspectItem(divideWorryLevel, mcm);
+                    monkeyPlayers[item.getObjective()].catchItem(item.getItem());
+                }
+            }            
+        }
+        Arrays.sort(monkeyPlayers);
+
+        System.out.println("Round rounds");
+        System.out.println(monkeyPlayers[0].toString());
+        System.out.println(monkeyPlayers[1].toString());
+        System.out.println(monkeyPlayers[2].toString());
+        System.out.println(monkeyPlayers[3].toString());
+
+        System.out.println("Part " + part + ": " + getMonkeyBusiness(monkeyPlayers[0], monkeyPlayers[1]));
+
+    }
+
+    private static long getMonkeyBusiness(Monkey monkey1, Monkey monkey2) {
+
+        return (long) monkey1.getInspectedItems() * monkey2.getInspectedItems();
+    }
+
+    private static long calculateLCM(Monkey[] allMonkeys) {
+
+        long MCM = 1;
+
+        for(Monkey monk : allMonkeys) {
+            MCM*=monk.getDivisionTest();
+        }
+
+        return MCM;
+    }
 }
--- day11/Monkey.java
+++ day11/Monkey.java
@@ -1,30 +1,35 @@
 package day11;
 
-import java.util.LinkedList;
+import java.util.List;
 
-public class Monkey {
+public class Monkey implements Comparable<Monkey>{
 
-    private LinkedList<Integer> items;
+    private List<Long> items;
     int divisionTest;
     int monkeyIfDivisible;
     int monkeyIfNotDivisible;
-    int operationAmount;
+    long operationAmount;
+    int inspectedItems;
     String operator;
+    int id;
 
-    public Monkey(LinkedList<Integer> items, String operator, int opeartionAmount, int test, int monkeyIfTrue, int monkeyIfFalse ) {
-        this.items = items ;
-        this.operator = operator;
-        this.operationAmount = opeartionAmount;
-        this.divisionTest = test;
-        this.monkeyIfDivisible = monkeyIfTrue;
-        this.monkeyIfNotDivisible = monkeyIfFalse;
+    public Monkey(int id) {
+        this.id = id;
     }
 
-    public Monkey() {
-
+    public int getDivisionTest() {
+        return this.divisionTest;
     }
 
-    public void setItems(LinkedList<Integer> items) {
+    public int getInspectedItems() {
+        return this.inspectedItems;
+    }
+
+    public int getItemsAmount() {
+        return items.size();
+    }
+
+    public void setItems(List<Long> items) {
         this.items = items;
     }
 
@@ -48,36 +53,61 @@ public class Monkey {
         this.monkeyIfNotDivisible = number;
     }
 
-    public Item executeOperation() {
+    public Item inspectItem(boolean divideWorryLevel, long MCM) {
 
         int objective;
-        int item = this.items.pollFirst();
-        objective = ((item = getOperationResult(item) % divisionTest) == 0)? monkeyIfDivisible : monkeyIfNotDivisible;
+        long item = divideWorryLevel? getOperationResult(this.items.remove(0)) / 3 : getOperationResult(this.items.remove(0));
+        objective = (item % divisionTest == 0)? monkeyIfDivisible : monkeyIfNotDivisible;
+        inspectedItems++;
+        item = item % MCM;
         return new Item(item, objective);
     }
 
-    private int getOperationResult(int itemValue) {
+    private long getOperationResult(long itemValue) {
         
-        if(this.operator.equals("*")) {
-            itemValue*=operationAmount;
-        } else if(this.operator.equals("+")) {
-            itemValue+=operationAmount;
+        switch (this.operator){
+            case "^2": 
+                itemValue*= itemValue;
+                break;
+            case "*": 
+                itemValue*=operationAmount;
+                break;
+            case "+":
+                itemValue+=operationAmount;
+                break;
         }
         return itemValue;
     }
-    
+
+    public void catchItem(long itemValue) {
+        this.items.add(itemValue);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Monkey " + this.id + ": ");
+        for(int i=0; i<this.items.size(); i++) {
+            sb.append(items.get(i));
+            if(i < (this.items.size()-1)) sb.append(", ");            
+        }
+        sb.append(" - Inspected: ").append(this.inspectedItems + " items.");
+        return sb.toString();
+    }
+
+    public int compareTo(Monkey monkey) {
+        return (this.inspectedItems < monkey.getInspectedItems())? 1 : -1;
+    }
 }
 
 class Item {
-    private int item;
+    private long item;
     private int objective;
 
-    public Item(int item, int objective) {
+    public Item(long item, int objective) {
         this.item=item;
         this.objective=objective;
     }
 
-    public int getItem() {
+    public long getItem() {
         return this.item;
     }
 
